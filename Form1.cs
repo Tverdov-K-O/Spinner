f7c99d0812d55805a9413c25572d98620f8ff841
Tverdov-K-O <89507603+Tverdov-K-O@users.noreply.github.com>
2022-02-23 23:34:15 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spinner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Spinner.Geometric_Shape.Spinner spinner = new Spinner.Geometric_Shape.Spinner(new Point(this.Width/2,this.Height/2),g);
+            new Task(() => { spinner.Execute(); }).Start();
+        }
+    }
+}
---
+++ Geometric_Shape/Elips.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Spinner.Geometric_Shape
+{
+    class Spinner : ICommand
+    {
+        float radius = 10f, angularSpeed = 2f;
+        float angle = 0f;
+        Graphics g;
+        public Point pointOnForm { get; set; }
+        public Spinner(Point frm, Graphics _g)
+        {
+            pointOnForm = frm;
+            g = _g;
+        }
+
+        private void Paint()
+        {
+            
+            while (true)
+            {
+
+                SolidBrush solidBrushRed = new SolidBrush(Color.FromArgb(255, 255, 0, 0));
+                g.FillEllipse(solidBrushRed, pointOnForm.X - radius*2 + MathF.Cos(angle) * radius, pointOnForm.Y - radius*2 + MathF.Sin(angle) * radius, 20, 20);
+                g.DrawEllipse(Pens.Coral, pointOnForm.X - radius*2 + MathF.Cos(angle) * radius, pointOnForm.Y - radius*2 + MathF.Sin(angle) * radius, 20, 20);
+                angle = angle + 0.10f * angularSpeed;
+                if (angle >= 360f)
+                {
+                    angle = 0;
+                }
+                Thread.Sleep(20);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter = null) => true;
+        public void Execute(object parameter = null) => Paint();
+    }
+}
